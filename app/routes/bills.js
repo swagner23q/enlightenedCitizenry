9ddbe7f36d9ca846941632026f2f0363af93a53e
sara <sara.wagner23q@gmail.com>
2016-04-05 14:22:35 -0700

    add viewable properties of bills clicked on

--- app/routes/bills.js
+++ app/routes/bills.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
  model: function() {
-   var url = 'http://congress.api.sunlightfoundation.com/bills?apikey=383f6003ce6c401cacb0424c620ad476';
+   var url = 'http://congress.api.sunlightfoundation.com/bills/search?apikey=383f6003ce6c401cacb0424c620ad476';
    return Ember.$.getJSON(url).then(function(responseJSON) {
      return responseJSON.results;
    });
