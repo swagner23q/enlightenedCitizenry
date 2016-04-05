4a9cd343968a3f47be74fb220a21cd644e0ad0ac
sara <sara.wagner23q@gmail.com>
2016-04-05 13:59:16 -0700

    add bill information

--- app/router.js
+++ app/router.js
@@ -10,6 +10,9 @@ Router.map(function() {
   this.route('legislatorsResults', {path: '/legislatorsResults/:zip'});
   this.route('house');
   this.route('senate');
+  this.route('houseSub', {path: '/houseSub/:committee.committee_id'});
+  this.route('bills');
+  this.route('billinfo', {path: '/billinfo/:bill_id'});
 });
 
 export default Router;
---
+++ app/routes/billinfo.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+ model: function() {
+   var url = 'http://congress.api.sunlightfoundation.com/bills?apikey=383f6003ce6c401cacb0424c620ad476';
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     return responseJSON.results;
+   });
+  }
+});
